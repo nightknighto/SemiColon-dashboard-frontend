b146f23a7ea74dd238bca1b348ff43b0548bd5e7
Abdelrahman Mostafa <71796506+bobo122b@users.noreply.github.com>
2023-05-02 23:58:06 +0300

    Statistics of Users (#2)
    * Added main UI components and header with navbar
    
    * Adding DropDown UI element
    
    * Finishing with most of basic UI elements
    
    * Adding Charts Components and checking their logic
    
    * After adding tracks
    
    * Update src/components/Charts/BarChart.tsx
    
    Co-authored-by: NightKnight <Ahmedatwa866@yahoo.com>
    
    * Update src/components/Charts/ChartItem.tsx
    
    Co-authored-by: NightKnight <Ahmedatwa866@yahoo.com>
    
    * Update src/components/Charts/PieChart.tsx
    
    Co-authored-by: NightKnight <Ahmedatwa866@yahoo.com>
    
    * Update src/components/Stats/Stats.tsx
    
    Co-authored-by: NightKnight <Ahmedatwa866@yahoo.com>
    
    * Resolving requested changes
    
    ---------
    
    Co-authored-by: Abdelrahman Mostafa <abdelrahman-mostafa@localhost.localdomain>
    Co-authored-by: NightKnight <Ahmedatwa866@yahoo.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from './components/UI/Header/Header'
+import Stats from './components/Stats/Stats'
+import Charts from './components/Charts/Charts'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const tracks = [
+    'Web Track-Basic',
+    'Web Track-Advanced',
+    'Embedded Track-Basic',
+    'Embedded Track-Advanced',
+  ]
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Charts />
+      <Stats tracks={tracks} />
     </>
   )
 }
---
+++ src/components/Stats/Stats.tsx
@@ -0,0 +1,24 @@
+import Button from '../UI/Button/Button'
+import Card from '../UI/Card/Card'
+import DropDown from '../UI/DropDown/DropDown'
+import classes from './Stats.module.css'
+import Track from './Track'
+
+const Stats = (props: { tracks: string[] }) => {
+  const tracksStats = props.tracks.map((track) => {
+    return {
+      name: track,
+      numParticipants: Math.floor(Math.random() * 100),
+    }
+  })
+  return (
+    <Card>
+      <h1>Tracks</h1>
+      {tracksStats.map((track) => (
+        <Track name={track.name} numParticipants={track.numParticipants} />
+      ))}
+    </Card>
+  )
+}
+
+export default Stats
