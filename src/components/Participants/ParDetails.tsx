4329924084a4c38e2d1619c490be4dee15d870d1
Abdelrahman Mostafa <boboelahlawy1234@hotmail.com>
2023-05-17 00:26:52 +0300

    Editing participants styling

--- src/components/Participants/ParDetails.tsx
+++ src/components/Participants/ParDetails.tsx
@@ -12,61 +12,75 @@ const ParDetails = ({
   onRejectHandler: (phone: string) => void
 }) => {
   return (
-    <div className={classes.details}>
-      <h2>{par.name}</h2>
-      <div className={classes.parData}>
-        <p>
-          <span className={classes.bold}>Email: </span>
-          {par.email}
-        </p>
-        <p>
-          <span className={classes.bold}>College ID: </span>
-          {par.collegeId}
-        </p>
-        <p>
-          <span className={classes.bold}>Phone: </span>
-          {par.phone}
-        </p>
-        <p>
-          <span className={classes.bold}>Year: </span>
-          {par.year}
-        </p>
-        <p>
-          <span className={classes.bold}>First Preference: </span>
-          {par.firstPreference}
-        </p>
-        <p>
-          <span className={classes.bold}>Reason: </span>
-          {par.firstPrefReason}
-        </p>
-        <p>
-          <span className={classes.bold}>Knowlege: </span>
-          {par.firstPrefKnowledge}
-        </p>
-        <p>
-          <span className={classes.bold}>Second Preference: </span>
-          {par.secondPreference}
-        </p>
-        <p>
-          <span className={classes.bold}>Reason: </span>
-          {par.secondPrefReason}
-        </p>
-        <p>
-          <span className={classes.bold}>Experience: </span>
-          {par.pastExperience}
-        </p>
-      </div>
-      <div className={classes.status}>
-        {par.acceptanceStatus && (
-          <p className={`${classes.pending} ${classes[par.acceptanceStatus]}`}>
-            {par.acceptanceStatus}
+    <div className={classes.parContainer}>
+      <div className={classes.details}>
+        <h2>{par.name}</h2>
+        <div className={classes.parData}>
+          <p>
+            <span className={classes.bold}>Email: </span>
+            {par.email}
           </p>
-        )}
+          <p>
+            <span className={classes.bold}>College ID: </span>
+            {par.collegeId}
+          </p>
+          <p>
+            <span className={classes.bold}>Phone: </span>
+            {par.phone}
+          </p>
+          <p>
+            <span className={classes.bold}>Year: </span>
+            {par.year}
+          </p>
+          <p>
+            <span className={classes.bold}>First Preference: </span>
+            {par.firstPreference}
+          </p>
+          <p>
+            <span className={classes.bold}>Reason: </span>
+            {par.firstPrefReason}
+          </p>
+          <p>
+            <span className={classes.bold}>Knowlege: </span>
+            {par.firstPrefKnowledge}
+          </p>
+          <p>
+            <span className={classes.bold}>Second Preference: </span>
+            {par.secondPreference}
+          </p>
+          <p>
+            <span className={classes.bold}>Reason: </span>
+            {par.secondPrefReason}
+          </p>
+          <p>
+            <span className={classes.bold}>Experience: </span>
+            {par.pastExperience}
+          </p>
+        </div>
+        <div className={classes.status}>
+          {par.acceptanceStatus && (
+            <p
+              className={`${classes.pending} ${classes[par.acceptanceStatus]}`}
+            >
+              {par.acceptanceStatus}
+            </p>
+          )}
+        </div>
       </div>
       <hr className={classes.line}></hr>
       <div className={classes.buttons}>
-        <Button onClick={onAcceptHandler.bind(null, par.phone)} className={classes.acceptBtn}>Accept</Button>
-        <Button onClick={onRejectHandler.bind(null, par.phone)} className={classes.rejectBtn}>Reject</Button>
+        <Button
+          onClick={onAcceptHandler.bind(null, par.phone)}
+          className={classes.acceptBtn}
+        >
+          Accept
+        </Button>
+        <Button
+          onClick={onRejectHandler.bind(null, par.phone)}
+          className={classes.rejectBtn}
+        >
+          Reject
+        </Button>
       </div>
     </div>
   )
