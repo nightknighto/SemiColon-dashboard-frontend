54bbc97ac319c5b316c89b2c008dd274e45b0353
Abdelrahman Mostafa <boboelahlawy1234@hotmail.com>
2023-06-22 02:05:39 +0300

    finalizing search feature

--- src/components/Participants/AllPars.tsx
+++ src/components/Participants/AllPars.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { parDataTypes } from '../../interfaces/parDataTypes'
 import { tracks } from '../../interfaces/tracks'
 import DropDown from '../UI/DropDown/DropDown'
@@ -14,20 +14,36 @@ const AllPars = ({
   onChoose: (id: string) => void
 }) => {
   const [filteredData, setFilteredData] = useState<parDataTypes[]>(data)
+  const [chosenTrack, setChosenTrack] = useState<string>('All')
+  const [searchPhone, setSearchPhone] = useState<string>('')
 
   let output
 
-  const onTrackChangeHandler = (track: string) => {
-    if (track === 'All') {
+  useEffect(() => {
+    if (chosenTrack === 'All') {
       setFilteredData(data)
     } else {
-      setFilteredData(data.filter((par) => par.firstPreference === track))
+      setFilteredData(data.filter((par) => par.firstPreference === chosenTrack))
     }
+  }, [chosenTrack, data])
+
+  const onTrackChangeHandler = (track: string) => {
+    if (track === 'All') {
+      setChosenTrack('All')
+    } else {
+      setChosenTrack(track)
+    }
+    setSearchPhone('')
   }
 
   const onSearchHandler = (phoneInput: string) => {
+    setSearchPhone(phoneInput)
     setFilteredData(
-      filteredData.filter((par) => par.phone.includes(phoneInput))
+      data.filter(
+        (par) =>
+          par.phone.includes(phoneInput) &&
+          (chosenTrack === 'All' || par.firstPreference === chosenTrack)
+      )
     )
   }
 
@@ -38,6 +54,7 @@ const AllPars = ({
           type="number"
           placeholder="search by phone"
           onChange={onSearchHandler}
+          value={searchPhone}
         />
         <DropDown choices={tracks} onChange={onTrackChangeHandler} />
         {filteredData.map((item) => (
--- src/components/UI/InputBar/InputBar.tsx
+++ src/components/UI/InputBar/InputBar.tsx
@@ -1,29 +1,40 @@
 import classes from './InputBar.module.css'
-import { ChangeEvent, ChangeEventHandler, HTMLInputTypeAttribute } from 'react'
+import {
+  ChangeEvent,
+  HTMLInputTypeAttribute,
+  LegacyRef,
+  forwardRef,
+} from 'react'
 
-const InputBar = ({
-  type,
-  placeholder,
-  onChange,
-}: {
-  type: HTMLInputTypeAttribute
-  placeholder?: string
-  onChange?: (value: string) => void
-}) => {
-  const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    if (onChange) {
-      onChange(event.target.value)
+const InputBar = forwardRef(
+  ({
+    type,
+    placeholder,
+    onChange,
+    value,
+  }: {
+    type: HTMLInputTypeAttribute
+    placeholder?: string
+    onChange?: (value: string) => void
+    value?: string
+  }, ref: LegacyRef<HTMLInputElement>) => {
+    const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+      if (onChange) {
+        onChange(event.target.value)
+      }
     }
-  }
 
-  return (
-    <input
-      type={type}
-      className={classes['in-bar']}
-      placeholder={placeholder}
-      onChange={onChangeInputHandler}
-    />
-  )
-}
+    return (
+      <input
+        type={type}
+        className={classes['in-bar']}
+        placeholder={placeholder}
+        onChange={onChangeInputHandler}
+        ref={ref}
+        value={value}
+      />
+    )
+  }
+)
 
 export default InputBar
