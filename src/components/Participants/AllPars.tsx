36fd8b436d515ca5806d8b67b6deeff8c646ba0b
Abdelrahman Mostafa <71796506+bobo122b@users.noreply.github.com>
2023-06-22 02:26:31 +0300

    Bobo (#18)
    * Adding history helper
    
    * Editing for using axios
    
    * getting JWT to work and rerouting if the user is not authenticated
    
    * bug fixes
    
    * build conflicts
    
    * finishing JWT
    
    * refactoring
    
    * fixing bugs
    
    * aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
    
    * Adding a hook for input
    
    * cleaning Login states with a useInput custom hook
    
    * adding form feedback
    
    * Adding more login feedback to please Atwa
    
    * Initiating new search feature
    
    * finalizing search feature

--- src/components/Participants/AllPars.tsx
+++ src/components/Participants/AllPars.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { parDataTypes } from '../../interfaces/parDataTypes'
 import { tracks } from '../../interfaces/tracks'
 import DropDown from '../UI/DropDown/DropDown'
 import classes from './AllPars.module.css'
 import ParItem from './ParItem'
+import InputBar from '../UI/InputBar/InputBar'
 
 const AllPars = ({
   data,
@@ -13,20 +14,48 @@ const AllPars = ({
   onChoose: (id: string) => void
 }) => {
   const [filteredData, setFilteredData] = useState<parDataTypes[]>(data)
+  const [chosenTrack, setChosenTrack] = useState<string>('All')
+  const [searchPhone, setSearchPhone] = useState<string>('')
 
   let output
 
-  const onTrackChangeHandler = (track: string) => {
-    if (track === 'All') {
+  useEffect(() => {
+    if (chosenTrack === 'All') {
       setFilteredData(data)
     } else {
-      setFilteredData(data.filter((par) => par.firstPreference === track))
+      setFilteredData(data.filter((par) => par.firstPreference === chosenTrack))
     }
+  }, [chosenTrack, data])
+
+  const onTrackChangeHandler = (track: string) => {
+    if (track === 'All') {
+      setChosenTrack('All')
+    } else {
+      setChosenTrack(track)
+    }
+    setSearchPhone('')
+  }
+
+  const onSearchHandler = (phoneInput: string) => {
+    setSearchPhone(phoneInput)
+    setFilteredData(
+      data.filter(
+        (par) =>
+          par.phone.includes(phoneInput) &&
+          (chosenTrack === 'All' || par.firstPreference === chosenTrack)
+      )
+    )
   }
 
   if (data[0]) {
     output = (
       <div>
+        <InputBar
+          type="number"
+          placeholder="search by phone"
+          onChange={onSearchHandler}
+          value={searchPhone}
+        />
         <DropDown choices={tracks} onChange={onTrackChangeHandler} />
         {filteredData.map((item) => (
           <ParItem
