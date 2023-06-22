8c504f95e5d6414b1edff94ca2b29f6a74e513b6
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2023-06-22 23:46:41 +0300

    add basic file layout

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -1,6 +1,33 @@
+import { useEffect, useState } from "react";
+import Card from "../UI/Card/Card";
+import { authHeader } from "../../helpers/auth";
+import { useNavigate } from "react-router-dom";
+import AllUsers from "./AllUsers";
+import classes from './Admin.module.css'
+import UserDetails from "./UserDetails";
+
 const Admin = () => {
+    const nav = useNavigate()
+    const [userData, setUserData] = useState(null);
+    
+
+
+    useEffect(() => {
+        const hdrs = authHeader()
+        if (hdrs) {
+            fetch("https://semicolon-registration-backend.onrender.com/user/getAll", {headers: hdrs})
+            .then(data => data.json())
+            .then(data => setUserData(data));
+        } else {
+            nav("/login")
+        }
+    }, []);
+
     return (
-        <h1>Admin dashboard page</h1>
+        <Card className={classes['admin-container']}>
+            <AllUsers />
+            <UserDetails />
+        </Card>
     );
 }
  
---
+++ src/components/Admin/AllUsers.tsx
@@ -0,0 +1,7 @@
+import classes from './AllUsers.module.css'
+
+const AllUsers = () => {
+    return <div className={classes['all-users']}></div>;
+}
+ 
+export default AllUsers;
