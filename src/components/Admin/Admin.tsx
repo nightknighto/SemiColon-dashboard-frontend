49b9ecb67d8551863c63138a79dabada2791df19
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2023-06-23 00:13:32 +0300

    add User type, add AllUsers component

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -5,27 +5,46 @@ import { useNavigate } from "react-router-dom";
 import AllUsers from "./AllUsers";
 import classes from './Admin.module.css'
 import UserDetails from "./UserDetails";
+import { User } from "../../types/User";
 
 const Admin = () => {
     const nav = useNavigate()
-    const [userData, setUserData] = useState(null);
-    
-
+    const [chosenUser, setChosenUser] = useState<User>({
+        _id: '',
+        active: false,
+        password: '',
+        phone: '',
+        role: '',
+        username: ''
+    });
+    const [userData, setUserData] = useState<User[]>([]);
 
     useEffect(() => {
         const hdrs = authHeader()
         if (hdrs) {
             fetch("https://semicolon-registration-backend.onrender.com/user/getAll", {headers: hdrs})
             .then(data => data.json())
-            .then(data => setUserData(data));
+            .then(data => {
+                setUserData(data);
+                console.log(data);
+            });
         } else {
             nav("/login")
         }
     }, []);
 
+    const onChoose = (id: string) => {
+        for (const user of userData) {
+          if (user._id === id) {
+            setChosenUser(user)
+            return
+          }
+        }
+      }
+
     return (
         <Card className={classes['admin-container']}>
-            <AllUsers />
+            <AllUsers data={userData} onChoose={onChoose}/>
             <UserDetails />
         </Card>
     );
