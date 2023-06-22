103cb566336bda7f30a4cd837e22b3b33afca059
Abdelrahman Mostafa <boboelahlawy1234@hotmail.com>
2023-06-23 02:11:24 +0300

    Adding latest changes in logout drop menu

--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -1,14 +1,36 @@
 import { Link } from 'react-router-dom'
 import classes from './Header.module.css'
 import { onLogout } from '../../../helpers/auth'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretDown, faUserCircle } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
 
 const Header = () => {
+  const [dropBarShow, setDropBarShow] = useState<boolean>(false)
+
+  const onBarToggle = () => {
+    setDropBarShow(!dropBarShow)
+  }
+
   return (
     <header>
-      <nav className={classes['main-nav']}>
-        <Link to="/login" className={classes['logout']} onClick={onLogout}>
-          Logout
-        </Link>
+      <nav className={classes['main-nav']} onClick={onBarToggle}>
+        <div className={classes['user-d']}>
+          <FontAwesomeIcon
+            icon={faUserCircle}
+            className={classes['usrCircle']}
+          />
+          <FontAwesomeIcon icon={faCaretDown} rotation={270} />
+        </div>
+        {dropBarShow && (
+          <ul className={classes['logout-dropdown']}>
+            <li>
+              <Link to="/login" onClick={onLogout}>
+                Logout
+              </Link>
+            </li>
+          </ul>
+        )}
         <ul>
           <li>
             <Link to="/stats">Stats</Link>
