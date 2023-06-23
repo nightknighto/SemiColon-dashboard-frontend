811e8cf59318973e11f85ef4a0794be09f4a4673
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2023-06-23 20:23:08 +0300

    add de/activate buttons

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -6,6 +6,7 @@ import AllUsers from "./AllUsers";
 import classes from './Admin.module.css'
 import UserDetails from "./UserDetails";
 import { User } from "../../types/User";
+import axios from "axios";
 
 const Admin = () => {
     const nav = useNavigate()
@@ -39,10 +40,47 @@ const Admin = () => {
         }
       }
 
+      const deactivateUser = async (id: string) => {
+        try {
+          const headers = authHeader()
+          if (headers) {
+            await axios.patch(
+              'https://semicolon-registration-backend.onrender.com/user/deactivate/'+id,
+              {},
+              {
+                headers,
+              }
+            )
+          } else {
+            nav('/login')
+          }
+        } catch (err) {
+          alert(`Error occured: ${err}`)
+        }
+      }
+      const activateUser = async (id: string) => {
+        try {
+          const headers = authHeader()
+          if (headers) {
+            await axios.patch(
+              'https://semicolon-registration-backend.onrender.com/user/activate/'+id,
+              {},
+              {
+                headers,
+              }
+            )
+          } else {
+            nav('/login')
+          }
+        } catch (err) {
+          alert(`Error occured: ${err}`)
+        }
+      }
+
     return (
         <Card className={classes['admin-container']}>
             <AllUsers data={userData} onChoose={onChoose}/>
-            <UserDetails user={chosenUser}/>
+            <UserDetails user={chosenUser} activateUser={activateUser} deactivateUser={deactivateUser}/>
         </Card>
     );
 }
