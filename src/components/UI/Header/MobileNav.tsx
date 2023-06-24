c314fd7a46c249ba1c6c7b9789fc374094cf513b
Abdelrahman Mostafa <boboelahlawy1234@hotmail.com>
2023-06-24 19:32:51 +0300

    finalizing MobileNav

--- src/components/UI/Header/MobileNav.tsx
+++ src/components/UI/Header/MobileNav.tsx
@@ -1,28 +1,45 @@
 import classes from './MobileNav.module.css'
 import StackedBtn from '../StackedBtn/StackedBtn'
 import { Link } from 'react-router-dom'
-import { useRef, useState } from 'react'
+import { MouseEventHandler, useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 
 const MobileNav = () => {
-  const [dialogOpen, setDialogOpen] = useState(false)
   const dialogRef = useRef<HTMLDialogElement>(null)
 
   const onModalShow = () => {
     dialogRef.current?.showModal()
-    setDialogOpen(true)
   }
 
   const onModalClose = () => {
     dialogRef.current?.close()
-    setDialogOpen(false)
+  }
+
+  const onBackDropClick: MouseEventHandler<HTMLDialogElement> = (
+    event: React.MouseEvent<HTMLDialogElement>
+  ) => {
+    const rect = dialogRef.current?.getBoundingClientRect()
+    if (rect) {
+      const isInDialog =
+        rect.top <= event.clientY &&
+        event.clientY <= rect.top + rect.height &&
+        rect.left <= event.clientX &&
+        event.clientX <= rect.left + rect.width
+      if (!isInDialog) {
+        dialogRef.current?.close()
+      }
+    }
   }
 
   return (
     <nav className={classes['mobile-nav']}>
       <StackedBtn onClick={onModalShow} />
-      <dialog ref={dialogRef} className={classes['dia']}>
+      <dialog
+        ref={dialogRef}
+        className={classes['dia']}
+        onClick={onBackDropClick}
+      >
         <ul className={classes['dia-elements']}>
           <li>
             <Link to="/stats" onClick={onModalClose}>
